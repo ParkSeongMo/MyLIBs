6423bae706f0c0a5161332a994bc286c61cf85c8
ParkSeongMo <parksmo83@gmail.com>
2023-05-04 17:08:40 +0900

    폴더 구조 변경, Needle 스크립트 추가

---
+++ MyLIBs/MyLIBs/Source/Presentation/Scenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLIBs
+//
+//  Created by Seongmo Park on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
