7df051b3361324604b6053a69314a460ad7f5c50
ParkSeongMo <parksmo83@gmail.com>
2023-05-04 16:01:18 +0900

    프로젝트 생성

---
+++ MyLIBs/MyLIBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLIBs
+//
+//  Created by Seongmo Park on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
